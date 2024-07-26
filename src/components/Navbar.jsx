811208a06360120cf18117d1fcdc57a1ea4f2bf1
Promish Gurung <promishgrg98@gmail.com>
2024-07-26 12:16:40 +0545

    style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,34 @@ import React from 'react'
 import './navbar.css'
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../images/gym-logo.png'
-import {links} from '../data'
+import { links } from '../data'
 import { FaBars } from "react-icons/fa6";
 
 function Navbar() {
-  return (
-    <nav className='container nav_container'>
-        <Link to={'/'} className='logo'>
-            <img src={Logo} alt="Logo" />
-        </Link>
-        <ul className="nav_links">
-            {
-                links.map(({name, path}, index) => {
-                    return (
-                        <li>
-                            <NavLink to={path}>{name}</NavLink>
-                        </li>
-                    )
-                })
-            }
-        </ul>
-        <button className="nav_toggle-btn">
-            <FaBars />
-        </button>
-    </nav>
-  )
+    return (
+        <nav>
+            <div className="container nav_container">
+                <Link to={'/'} className='logo'>
+                    <img src={Logo} alt="Logo" />
+                </Link>
+                <ul className="nav_links">
+                    {
+                        links.map(({ name, path }, index) => {
+                            return (
+                                <li>
+                                    <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{name}</NavLink>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <button className="nav_toggle-btn">
+                    <FaBars />
+                </button>
+            </div>
+
+        </nav>
+    )
 }
 
 export default Navbar

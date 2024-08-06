07c5f4022521f3613954aad5e400c7539d99fe27
Promish Gurung <promishgrg98@gmail.com>
2024-08-06 10:19:16 +0545

    style programs component

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -7,28 +7,28 @@ import { Link } from 'react-router-dom';
 import { AiFillCaretRight } from "react-icons/ai";
 
 function Programs() {
-  return (
-    <section className="programs">
-        <div className="programs_container container">
-            <SectionHead icon={<FaCrown />} title={'Programs'} />
-        </div>
+    return (
+        <section className="programs">
+            <div className="programs_container container">
+                <SectionHead icon={<FaCrown />} title={'Programs'} />
 
-        <div className="programs_wrapper">
-            {
-                programs.map(({id, icon, title, info, path})=> {
-                    return (
-                        <Card className={'programs_program'} key={id}>
-                            <span>{icon}</span>
-                            <h4>{title}</h4>
-                            <small>{info}</small>
-                            <Link to={path} className='btn sm'>Learn More <AiFillCaretRight /></Link>
-                        </Card>
-                    )
-                })
-            }
-        </div>
-    </section>
-  )
+                <div className="programs_wrapper">
+                    {
+                        programs.map(({ id, icon, title, info, path }) => {
+                            return (
+                                <Card className={'programs_program'} key={id}>
+                                    <span>{icon}</span>
+                                    <h4>{title}</h4>
+                                    <small>{info}</small>
+                                    <Link to={path} className='btn sm'>Learn More <AiFillCaretRight /></Link>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Programs

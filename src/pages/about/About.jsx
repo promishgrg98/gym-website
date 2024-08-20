00088b0df159c8d33e82d16144bd9570bb582b9f
Promish Gurung <promishgrg98@gmail.com>
2024-08-20 17:57:44 +0545

    create Header Component

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './about.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_1.jpg'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <Header title={'About Us'} image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nobis. Sapiente voluptatum alias nulla soluta? Quaerat at magnam necessitatibus quasi.
+      </Header>
+    </div>
   )
 }
 

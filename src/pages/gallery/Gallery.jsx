b8087a5f1365ed067e1252a3d106976f4d72b58b
Promish Gurung <promishgrg98@gmail.com>
2024-08-26 20:57:48 +0545

    add content for gallery page

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './gallery.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_3.jpg'
 
 function Gallery() {
+
+  const images = [];
+
+  for (let i = 1; i <= 15; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+
   return (
-    <div>Gallery</div>
+    <div>
+      <Header title={'Our Gallery'} image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius nesciunt nihil magnam amet itaque numquam adipisci ea omnis dolorem soluta necessitatibus in asperiores ipsum recusandae iure assumenda odit, quod labore.
+      </Header>
+
+      <section className="gallery">
+        <div className="container gallery_container">
+          {
+            images.map((image, index) => {
+              return <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            })
+          }
+        </div>
+      </section>
+    </div>
   )
 }
 

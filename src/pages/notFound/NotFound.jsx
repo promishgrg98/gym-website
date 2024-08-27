2c8cc17fa136788728a7a43c63e0760571598419
Promish Gurung <promishgrg98@gmail.com>
2024-08-27 11:06:18 +0545

    add page not found

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './notFound.css'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <section>
+      <div className="container notfound_container">
+        <h2>Page Not Found</h2>
+        <Link to={'/'} className='btn'>Go Back to Home</Link>
+      </div>
+    </section>
   )
 }
 
